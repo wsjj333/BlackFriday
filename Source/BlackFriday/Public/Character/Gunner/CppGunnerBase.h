35d993c8ee35d03509544389760c1fdabf7ffb94
shr0574 <shr0574@naver.com>
2025-12-29 14:40:20 +0900

    251229

--- Source/BlackFriday/Public/Character/Gunner/CppGunnerBase.h
+++ Source/BlackFriday/Public/Character/Gunner/CppGunnerBase.h
@@ -6,11 +6,11 @@
 #include "Character/Common/BFCharacterBase.h"
 #include "CppGunnerBase.generated.h"
 
-class USpringArmComponent;
-class UCameraComponent;
-class UInputMappingContext;
-class UInputAction;
-struct FInputActionValue;
+//class USpringArmComponent;
+//class UCameraComponent;
+//class UInputMappingContext;
+//class UInputAction;
+//struct FInputActionValue;
 
 /**
  * 
@@ -20,56 +20,56 @@ class BLACKFRIDAY_API ACppGunnerBase : public ABFCharacterBase
 {
 	GENERATED_BODY()
 	
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	USpringArmComponent* CameraBoom;
+	///** Camera boom positioning the camera behind the character */
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	//USpringArmComponent* CameraBoom;
 
-	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	UCameraComponent* FollowCamera;
+	///** Follow camera */
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	//UCameraComponent* FollowCamera;
 
-	/** MappingContext */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputMappingContext* DefaultMappingContext;
+	///** MappingContext */
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	//UInputMappingContext* DefaultMappingContext;
 
-	/** Jump Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* JumpAction;
+	///** Jump Input Action */
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	//UInputAction* JumpAction;
 
-	/** Move Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* MoveAction;
+	///** Move Input Action */
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	//UInputAction* MoveAction;
 
-	/** Look Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* LookAction;
+	///** Look Input Action */
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	//UInputAction* LookAction;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hand", meta = (AllowPrivateAccess = "true"))
-	bool IsHandMoving;
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hand", meta = (AllowPrivateAccess = "true"))
+	//bool IsHandMoving;
 
 
 public:
 	ACppGunnerBase();
-
-protected:
-
-	/** Called for movement input */
-	void Move(const FInputActionValue& Value);
-
-	/** Called for looking input */
-	void Look(const FInputActionValue& Value);
-
-
-protected:
-	// APawn interface
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	// To add mapping context
-	virtual void BeginPlay();
-
-public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+//
+//protected:
+//
+//	/** Called for movement input */
+//	void Move(const FInputActionValue& Value);
+//
+//	/** Called for looking input */
+//	void Look(const FInputActionValue& Value);
+//
+//
+//protected:
+//	// APawn interface
+//	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+//
+//	// To add mapping context
+//	virtual void BeginPlay();
+//
+//public:
+//	/** Returns CameraBoom subobject **/
+//	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+//	/** Returns FollowCamera subobject **/
+//	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 };
