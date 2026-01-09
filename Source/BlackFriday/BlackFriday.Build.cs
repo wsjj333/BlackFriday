2bfc89b2efcb58c45ac4933fde9e9e0f197aadf5
wsjj333 <wsjj333@naver.com>
2026-01-09 13:30:32 +0900

    세션 생성, 클라이언트 접속 가능

--- Source/BlackFriday/BlackFriday.Build.cs
+++ Source/BlackFriday/BlackFriday.Build.cs
@@ -11,7 +11,9 @@ public class BlackFriday : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[]
 		{
 			"BlackFriday",
-			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
+			"OnlineSubsystem", "OnlineSubsystemUtils"
 		});
+		DynamicallyLoadedModuleNames.Add("OnlineSubsystemNull");
 	}
 }
