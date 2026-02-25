4605efb77c0fe4ba70762312ab879d0b3f446957
fusigi33 <ben044512@gmail.com>
2026-02-25 13:58:47 +0900

    refactor(Cart): 회전 조작감 수정

--- Source/BlackFriday/Private/Vehicle/Cart/BFCartPawn.cpp
+++ Source/BlackFriday/Private/Vehicle/Cart/BFCartPawn.cpp
@@ -390,7 +390,8 @@ void ABFCartPawn::SetAccelAxis_Local(float Axis)
 
 void ABFCartPawn::SetSteerAxis_Local(float Axis)
 {
-	Rep_DriftRotation.Yaw = FMath::Sign(Axis) * 25.0f;
+	// TODO: 매직넘버 수정(25도 회전을 의도, 테스트를 위해 잠시 50도 사용)
+	Rep_DriftRotation.Yaw = FMath::Sign(Axis) * 50.0f;
 	Rep_SteerAxis = Rep_SteeringMultiplier == 2.0f 
 	? Axis
 	: FMath::Max(Rep_DriftSteer, FMath::Abs(Axis)) * FMath::Sign(Rep_DriftRotation.Yaw);
@@ -405,8 +406,8 @@ void ABFCartPawn::SetSteerAxis_Server(float Axis)
 {
 	if (!HasAuthority()) return;
 	
-	// TODO: 매직넘버 수정(25도 회전을 의도함)
-	Rep_DriftRotation.Yaw = FMath::Sign(Axis) * 25.0f;
+	// TODO: 매직넘버 수정(25도 회전을 의도, 테스트를 위해 잠시 50도 사용)
+	Rep_DriftRotation.Yaw = FMath::Sign(Axis) * 50.0f;
 	
 	Rep_SteerAxis = Rep_SteeringMultiplier == 2.0f 
 	? Axis
