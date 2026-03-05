17430dab7fd74f7dcd239d172f0bb10f33ea05f6
fusigi33 <ben044512@gmail.com>
2026-03-05 14:44:54 +0900

    Refactor: BFCartOverlapDetectorComponent

--- Source/BlackFriday/Private/Component/Pusher/BFCartOverlapDetectorComponent.cpp
+++ Source/BlackFriday/Private/Component/Pusher/BFCartOverlapDetectorComponent.cpp
@@ -1,8 +1,13 @@
 #include "Component/Pusher/BFCartOverlapDetectorComponent.h"
 
-#include "Character/Pusher/BFPusher.h"
+// Engine
 #include "Components/SphereComponent.h"
 #include "Net/UnrealNetwork.h"
+
+// Character
+#include "Character/Pusher/BFPusher.h"
+
+// Vehicle
 #include "Vehicle/Cart/BFCartPawn.h"
 
 UBFCartOverlapDetectorComponent::UBFCartOverlapDetectorComponent()
@@ -14,11 +19,10 @@ UBFCartOverlapDetectorComponent::UBFCartOverlapDetectorComponent()
 	OverlapSphere->InitSphereRadius(SphereRadius);
 	OverlapSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	OverlapSphere->SetCollisionObjectType(ECC_WorldDynamic);
-
+	
 	OverlapSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
-	OverlapSphere->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
-	// 만약 Cart의 ObjectType이 Pawn/PhysicsBody 등이라면 위 채널을 Cart에 맞춰 바꿔야 함.
-
+	OverlapSphere->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+	
 	OverlapSphere->SetGenerateOverlapEvents(true);
 }
 
@@ -34,9 +38,9 @@ void UBFCartOverlapDetectorComponent::BeginPlay()
 	{
 		OverlapSphere->AttachToComponent(Root, FAttachmentTransformRules::KeepRelativeTransform);
 	}
-
+	
 	OverlapSphere->SetSphereRadius(SphereRadius);
-
+	
 	OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &UBFCartOverlapDetectorComponent::HandleBeginOverlap);
 	OverlapSphere->OnComponentEndOverlap.AddDynamic(this, &UBFCartOverlapDetectorComponent::HandleEndOverlap);
 
@@ -57,10 +61,14 @@ void UBFCartOverlapDetectorComponent::HandleBeginOverlap(
 {
 	AActor* Owner = GetOwner();
 	if (!Owner || !Owner->HasAuthority())
+	{
 		return;
+	}
 
 	if (!OtherActor || OtherActor == Owner)
+	{
 		return;
+	}
 	
 	if (ABFCartPawn* Cart = Cast<ABFCartPawn>(OtherActor))
 	{
@@ -124,6 +132,8 @@ void UBFCartOverlapDetectorComponent::ForceRecheckOverlap_ServerOnly()
 	AActor* Owner = GetOwner();
 	if (!Owner || !Owner->HasAuthority())
 		return;
+	
+	if (!OverlapSphere) return;
 
 	TArray<AActor*> Actors;
 	OverlapSphere->GetOverlappingActors(Actors, ABFCartPawn::StaticClass());
